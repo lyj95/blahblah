e7535da16a7ab117842e8cfec6b121174078c861
lyj95 <lyj95>
2020-02-12 16:28:31 +0900

    LogFilter 오류 filter 주석처리

--- BlahBlah/src/main/java/com/blah/common/filter/LogFilter.java
+++ BlahBlah/src/main/java/com/blah/common/filter/LogFilter.java
@@ -1,5 +1,38 @@
 package com.blah.common.filter;
 
-public class LogFilter {
+import java.io.IOException;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogFilter implements Filter {
+
+	private Logger logger = LoggerFactory.getLogger(LogFilter.class);
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
