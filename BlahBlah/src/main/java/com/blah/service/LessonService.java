daec71b63a90547816277f341529f6619b9a0ad6
yunachu <gooddays1202@naver.com>
2020-02-18 12:03:55 +0900

    강의게시판 수정

--- BlahBlah/src/main/java/com/blah/service/LessonService.java
+++ BlahBlah/src/main/java/com/blah/service/LessonService.java
@@ -3,6 +3,7 @@ package com.blah.service;
 import java.util.List;
 
 import com.blah.vo.LessonVo;
+import com.blah.vo.ReviewVo;
 
 public interface LessonService {
 	List<LessonVo> selectList();
@@ -10,6 +11,15 @@ public interface LessonService {
 	int insert(LessonVo vo);
 	public int update(LessonVo vo);
 	public int delete(int lessonNo);
+	List<LessonVo> searchKeyword(String keyword);
+	List<LessonVo> searchLICENSE();
+	List<LessonVo> searchSPEAKING();
+	List<LessonVo> searchHighLevel();
+	List<LessonVo> searchMidLevel();
+	List<LessonVo> searchLowLevel();
+	int addReview(ReviewVo vo);
+	List<ReviewVo> selectReviewList(int lessonNo);
+
 	
 
 }
