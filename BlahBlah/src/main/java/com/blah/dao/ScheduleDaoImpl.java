b40ef4f5721b02c6b501f1410b8ee6ad850681f1
ssoob <ssb10214@naver.com>
2020-02-19 14:47:47 +0900

    캘린더 추가

--- BlahBlah/src/main/java/com/blah/dao/ScheduleDaoImpl.java
+++ BlahBlah/src/main/java/com/blah/dao/ScheduleDaoImpl.java
@@ -1,8 +1,33 @@
 package com.blah.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.blah.vo.CalendarVo;
 
 @Repository
 public class ScheduleDaoImpl implements ScheduleDao{
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	@Override
+	public List<CalendarVo> selectCalendar(String memberId) {
+		
+		List<CalendarVo> res = new ArrayList<CalendarVo>();
+		
+		try {
+			res = sqlSession.selectList(NAMESPACE+"selectCalendar", memberId);	
+		}catch(Exception e) {
+			System.out.println("[error] : schedule selectCalendar");
+			e.printStackTrace();
+		}
+				
+		return res;
+	}
 
 }
--- BlahBlah/src/main/java/com/blah/service/ScheduleService.java
+++ BlahBlah/src/main/java/com/blah/service/ScheduleService.java
@@ -1,5 +1,11 @@
 package com.blah.service;
 
+import java.util.List;
+
+import com.blah.vo.CalendarVo;
+
 public interface ScheduleService {
+	
+	public List<CalendarVo> selectCalendar(String memberId);
 
 }
