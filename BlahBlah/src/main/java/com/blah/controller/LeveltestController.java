b2aa3fe699c3a1c6192ec3cfb62cbf14d7102980
ssoob <ssb10214@naver.com>
2020-03-11 13:39:05 +0900

    레벨테스트

--- BlahBlah/src/main/java/com/blah/controller/LeveltestController.java
+++ BlahBlah/src/main/java/com/blah/controller/LeveltestController.java
@@ -1,6 +1,5 @@
 package com.blah.controller;
 
-import java.util.Locale;
 
 import javax.servlet.http.HttpSession;
 
@@ -8,44 +7,69 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.blah.service.LeveltestService;
+import com.blah.service.LeveltestServiceImpl;
 import com.blah.vo.LeveltestVo;
+import com.blah.vo.MemberVo;
 
 @Controller
 public class LeveltestController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Autowired
+	private LeveltestService service = new LeveltestServiceImpl();
 
-	
 	@RequestMapping(value = "/leveltestStart")
-	public String startLeveltest(Locale locale, Model model) {
-		
+	public String startLeveltest() {
 		logger.info("Start leveltest?");
-	
 		return "leveltest/leveltestStart";
 	}
-	
-	
+
 	@RequestMapping(value = "/leveltest")
-	public ModelAndView leveltest(Model model,LeveltestVo vo,HttpSession session) {
+	public String leveltest() {
 		logger.info("leveltest");
-			
-		ModelAndView mav = new ModelAndView("leveltest/leveltest");
-				
-		return mav;
+		return "leveltest/leveltest";
 	}
+
 	
-	@RequestMapping(value = "/leveltestResult")
-	public ModelAndView leveltestResult(Model model,LeveltestVo vo,HttpSession session) {
+	@RequestMapping(value = "/leveltestResult", method=RequestMethod.POST)
+	public ModelAndView leveltestResult(LeveltestVo vo, HttpSession session,
+			@RequestParam("q1") int q1, @RequestParam("q2") int q2, 
+			@RequestParam("q3") int q3, @RequestParam("q4") int q4, @RequestParam("q5") int q5) {
 		logger.info("leveltest Result");
-			
+		
+		MemberVo mvo = (MemberVo)session.getAttribute("login");
+		String memberId = mvo.getMemberId();
+		vo.setMemberId(memberId);
+		
+		int levelScore = q1+ q2 + q3 + q4 + q5;
+		
+		vo.setLevelScore(levelScore);
+		if(levelScore >= 80) {
+			vo.setMemberLevel("상");
+		}else if(levelScore < 80 && levelScore >= 60 ) {
+			vo.setMemberLevel("중");
+		}else {
+			vo.setMemberLevel("하");
+		}
+		
+		int res = service.insert(vo); 
+		
+		if (res > 0) {
+			logger.info("[leveltest]insert success");
+		} else {
+			logger.info("[leveltest]insert fail....");
+		}
+		
 		ModelAndView mav = new ModelAndView("leveltest/leveltestResult");
-				
-		return mav;
+		mav.addObject("vo", service.selectLevel(memberId));
+		  				  
+		return mav; 
 	}
-	
+
 }
--- BlahBlah/src/main/java/com/blah/service/LeveltestServiceImpl.java
+++ BlahBlah/src/main/java/com/blah/service/LeveltestServiceImpl.java
@@ -1,7 +1,28 @@
 package com.blah.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.blah.dao.LeveltestDao;
+import com.blah.vo.LeveltestVo;
+
+@Service
 public class LeveltestServiceImpl implements LeveltestService{
 	
+	@Autowired
+	private LeveltestDao dao;
+
+	@Override
+	public int insert(LeveltestVo vo) {
+		return dao.insert(vo);
+	}
+
+	@Override
+	public LeveltestVo selectLevel(String memberId) {
+		System.out.println("into service");
+		return dao.selectLevel(memberId);
+	}
+
+
 
 }
